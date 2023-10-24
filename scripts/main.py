4c61c6bb6a8f94a96111140a10279baa36b50473
NValsted <nicolaj.valsted@live.dk>
2023-10-24 22:06:52 +0200

    account for eclipsed orbit solar panel sun view factor

--- scripts/main.py
+++ scripts/main.py
@@ -7,8 +7,10 @@ from matplotlib import pyplot as plt
 from poliastro.bodies import Earth
 from poliastro.twobody import Orbit
 from skyfield.api import load
+from tqdm import tqdm
 
 LAUNCH_DATE = datetime(2024, 7, 1, tzinfo=timezone.utc)
+
 EARTH_MU = (3.986004418 * (10**14)) << (u.m**3 / u.s**2)  # type: ignore
 MEAN_MOTION = 15.09 << 1 / u.day  # type: ignore
 
@@ -75,7 +77,7 @@ def main():
 
     pos = []
     power = []
-    for i in range(int(60 * 10)):
+    for i in tqdm(range(int(60 * 10))):
         crr_datetime = LAUNCH_DATE + timedelta(minutes=i)
         t = ts.utc(crr_datetime.year, crr_datetime.month, crr_datetime.day)
         sat_orbit = sat_orbit.propagate(1 << u.minute)  # type: ignore
@@ -84,38 +86,56 @@ def main():
         earth_pos = earth.at(t)  # type: ignore
         sun_pos = sun.at(t)  # type: ignore
 
-        vector_to_sat = sat_orbit.r
-        vector_to_sun = sun_pos.position.to(u.km) - earth_pos.position.to(u.km)
+        v_earth_to_sat = sat_orbit.r
+        v_earth_to_sun = sun_pos.position.to(u.km) - earth_pos.position.to(u.km)
+        v_sat_to_earth = -v_earth_to_sat
+        v_sat_to_sun = sun_pos.position.to(u.km) - sat_orbit.r
+        sat_direction = v_sat_to_earth  # satellite always pointing towards earth
 
-        angle = np.arccos(
+        orbit_sun_angle = np.arccos(
             np.dot(
-                vector_to_sat / np.linalg.norm(vector_to_sat),
-                vector_to_sun / np.linalg.norm(vector_to_sun),
+                v_earth_to_sat / np.linalg.norm(v_earth_to_sat),
+                v_earth_to_sun / np.linalg.norm(v_earth_to_sun),
             )
         )
+        shadow_angle_tresh = np.arctan(Earth.R / np.linalg.norm(v_earth_to_sat)) + (
+            np.pi / 2 << u.rad
+        )
+
+        if orbit_sun_angle >= shadow_angle_tresh:
+            sun_view_factor = 0
+        else:
+            sun_view_factor = max(
+                np.dot(
+                    sat_direction / np.linalg.norm(sat_direction),
+                    v_sat_to_sun / np.linalg.norm(v_sat_to_sun),
+                ).to_value(),
+                0,
+            )
+
         power.append(
             SOLAR_IRRADIANCE
             * SOLAR_CELL_EFFICIENCY
             * SOLAR_PANEL_AREA
-            * angle.to_value()
+            * sun_view_factor
         )
 
-    fig = plt.figure()
-    ax = fig.add_subplot(projection="3d")
-    ax.scatter(  # CubeSat
-        [p[0].to_value() for p in pos],
-        [p[1].to_value() for p in pos],
-        [p[2].to_value() for p in pos],
-        alpha=[p / len(pos) for p in range(len(pos))],
-    )
-    ax.scatter(  # Earth
-        [0],
-        [0],
-        [0],
-        color="green",
-        s=200,
-    )
-    # plt.plot([p.to_value() for p in power])
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection="3d")
+    # ax.scatter(  # CubeSat
+    #     [p[0].to_value() for p in pos],
+    #     [p[1].to_value() for p in pos],
+    #     [p[2].to_value() for p in pos],
+    #     alpha=[p / len(pos) for p in range(len(pos))],
+    # )
+    # ax.scatter(  # Earth
+    #     [0],
+    #     [0],
+    #     [0],
+    #     color="green",
+    #     s=200,
+    # )
+    plt.plot([p.to_value() for p in power])
     plt.show()
 
 
